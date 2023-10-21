962487625f47222d328ce4ce0d969c092e7dbc8c
Christopher Harris <xixonia@gmail.com>
2023-10-21 04:25:38 +0000

    .

--- include/coro/concepts/executor.hpp
+++ include/coro/concepts/executor.hpp
@@ -7,15 +7,19 @@
 
 namespace coro::concepts
 {
+
+template<typename type>
+concept awaiter_or_awaitable = coro::concepts::awaiter<type> || coro::concepts::awaitable<type>;
+
 template<typename type>
 concept executor = requires(type t, std::coroutine_handle<> c)
 {
     {
         t.schedule()
-        } -> coro::concepts::awaiter;
+        } -> coro::concepts::awaiter_or_awaitable;
     {
         t.yield()
-        } -> coro::concepts::awaiter;
+        } -> coro::concepts::awaiter_or_awaitable;
     {
         t.resume(c)
         } -> std::same_as<void>;
